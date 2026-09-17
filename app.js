7f3f78b25a1a0bbdaa6a752d808b598b96a3b4c0
glckfndr <obulan65@gmail.com>
2026-09-17 15:38:11 +0300

    config: add linter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+   console.log('Did we get here?');
    res.send('Works on my machine.');
 });
 
--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const app = require('./app');
 
 const port = 3000;
 app.listen(port, () => {
-   console.log(`Server listening on port ${port}`);
+   let msg = `Server listening on port ${port}`;
+   //eslint-disable-next-line no-console
+   console.log(msg);
 });
