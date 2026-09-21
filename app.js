2f4dfc3f9f584a4f0869bc72944ffdf0f2c446e1
glckfndr <obulan65@gmail.com>
2026-09-21 18:03:38 +0300

    chore: configure formatting and Docker support

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+   //eslint-disable-next-line no-console
    console.log('Did we get here?');
    res.send('Works on my machine.');
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
    const msg = `Server listening on port ${port}`;
    //eslint-disable-next-line no-console
    console.log(msg);
